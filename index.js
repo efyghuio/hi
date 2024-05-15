ff05717f1edde6d1210bb24d79b8f91ef09a4b4a
efyghuio <dsjcanytariq913@hotmail.com>
2024-05-15 07:56:14 +0700

    update task

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function isValidBST(root) {
-  let prev = null;
-  return inorder(root);
-  function inorder(node) {
-    if (!node) return true;
-    if (!inorder(node.left)) return false;
-    if (prev !== null && node.val <= prev) return false;
-    prev = node.val;
-    return inorder(node.right);
+const factorialRecursive = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-}
+  return n * factorialRecursive(n - 1);
+};
